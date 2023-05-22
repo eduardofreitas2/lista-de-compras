323795c9e6a993fa5d9a3d83c8a3b5de32bc7f89
Eduardo Freitas <eduardofragadefreitas@gmail.com>
2023-05-22 20:26:12 -0300

    include tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
+import './global.css'
+
 export function App() {
   return (
-    <h2>Hello, World!</h2>
+    <div className='container mx-auto px-4 flex items-center justify-center'>
+      <h2>Hello, World!</h2>
+    </div>
   )
 }
